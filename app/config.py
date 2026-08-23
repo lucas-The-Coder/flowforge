20b8241942758755330d464b0423a3be57d63961
Luke <denniswluke@outlook.com>
2026-08-23 16:27:21 +0200

    config.py fix

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     app_name: str = "FlowForge"
@@ -9,11 +10,11 @@ class Settings(BaseSettings):
         "postgresql+psycopg://postgres:postgres@localhost:5432/flowforge"
     )
 
-
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
+
 
 settings = Settings()
